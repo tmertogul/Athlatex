9362ed39cf794f638a8b9cbfcee0706f6ebb36f3
CSchrisD <crduran@ucsc.edu>
2015-11-25 17:12:19 -0800

    board 11/25/15

---
+++ SlugLife_11:19:15/SlugLife/StudentEventsCell.swift
@@ -0,0 +1,19 @@
+//
+//  StudentEvents.swift
+//  SlugLife
+//
+//  Created by CDuran on 11/19/15.
+//  Copyright © 2015 Tim Mertogul. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class StudentEventsCell: PFTableViewCell {
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var college: UILabel!
+    @IBOutlet weak var descrip: UILabel!
+    @IBOutlet weak var datetime: UILabel!
+    
+}
